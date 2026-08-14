5ce80085a4980a3756693959c8fafe1389f2c424
Matheus de Almeida Cantarutti <cantaruttim@Matheuss-MacBook-Air.local>
2026-08-13 21:22:03 -0300

    code refactor

--- ms-service-credit/src/main/java/br/com/cantarutti/ms_credit_service/records/CompanyDataRequest.java
+++ ms-service-credit/src/main/java/br/com/cantarutti/ms_credit_service/records/CompanyDataRequest.java
@@ -5,23 +5,23 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class CompanyDataRequest {
 
     @JsonProperty("receita_anual")
-    private double receitaAnual;
+    private double receita_anual;
 
     @JsonProperty("divida_total")
-    private double dividaTotal;
+    private double divida_total;
 
     @JsonProperty("lucro_liquido")
-    private double lucroLiquido;
+    private double lucro_liquido;
 
     @JsonProperty("tempo_mercado")
-    private int tempoMercado;
+    private int tempo_mercado;
 
-    public double getReceitaAnual() { return receitaAnual; }
-    public void setReceitaAnual(double receitaAnual) { this.receitaAnual = receitaAnual; }
-    public double getDividaTotal() { return dividaTotal; }
-    public void setDividaTotal(double dividaTotal) { this.dividaTotal = dividaTotal; }
-    public double getLucroLiquido() { return lucroLiquido; }
-    public void setLucroLiquido(double lucroLiquido) { this.lucroLiquido = lucroLiquido; }
-    public int getTempoMercado() { return tempoMercado; }
-    public void setTempoMercado(int tempoMercado) { this.tempoMercado = tempoMercado; }
+    public double getReceitaAnual() { return receita_anual; }
+    public void setReceitaAnual(double receita_anual) { this.receita_anual = receita_anual; }
+    public double getDividaTotal() { return divida_total; }
+    public void setDividaTotal(double divida_total) { this.divida_total = divida_total; }
+    public double getLucroLiquido() { return lucro_liquido; }
+    public void setLucroLiquido(double lucro_liquido) { this.lucro_liquido = lucro_liquido; }
+    public int getTempoMercado() { return tempo_mercado; }
+    public void setTempoMercado(int tempo_mercado) { this.tempo_mercado = tempo_mercado; }
 }
